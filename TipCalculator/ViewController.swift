f76e1cc17408b321230a8d783150a80e8cf7dac6
Ervy <ervy@Ervys-MacBook-Air.local>
2018-12-28 15:03:31 -0800

    Initial Commit

---
+++ TipCalculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TipCalculator
+//
+//  Created by Ervy on 12/28/18.
+//  Copyright © 2018 Angelo m. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
